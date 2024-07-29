31390afacfca445a62c62cbe4a2db4c9071e600c
Magomed <barret1999@bk.ru>
2024-07-29 11:41:30 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import Cart from "./components/Cart"
 import './App.css';
+
+
+const questions=[
+  {
+    title:"React-это...?",
+    variants:["Библиотека","Фреймворк","Прилложение"],
+    correct:0,
+  },
+  {
+    title:'Компонент-это...',
+    variants:["Приложение","Часть приложения","Функция"]
+  },
+  {
+    title:"Что такое JSX?",
+    variants:["Это просто HTML","Это просто функция","Это тот же HTML,но с возможностью выполнять в JS"],
+    correct:3
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Cart/>
     </div>
   );
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Cart(){
+    return(
+        <div>
+            <input type="range"></input>
+            <h1></h1>
+            
+        </div>
+    )
+}
